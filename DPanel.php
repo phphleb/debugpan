b502ab415e16c683f6dbac3db79f6a9d6808f1e4
Foma Tuturov <fomiash@yandex.ru>
2019-05-01 14:21:52 +0300

    global upd

--- DPanel.php
+++ DPanel.php
@@ -13,6 +13,10 @@ use Hleb\Main\WorkDebug;
 class DPanel
 {
     use \DeterminantStaticUncreated;
+    
+    private static $queries = false;
+
+    private static $wdebug = false;
 
     public static function add($info)
     {
@@ -30,13 +34,13 @@ class DPanel
         foreach ($timing as $key => $value) {
 
             $hl_data_time .= "<div style='padding: 3px'>" . $key . ": " . $value . ($hl_preview > 0 ?
-                    " (+" . round($value - $hl_preview, 4) . ")" : "") . "</div>";
+            " (+" . round($value - $hl_preview, 4) . ")" : "") . "</div>";
 
             $hl_preview = $value;
         }
 
         $hl_this_route = self::this_block($info["block"]);
-
+        
         $hl_pr_updates = self::my_links();
 
         $hl_block_data = $info["block"];
@@ -67,7 +71,9 @@ class DPanel
             }
         }
 
-        $updates =
+        $orm_report = self::create_orm_report();
+
+        self::$queries = !empty($orm_report[0]);
 
         $cashe_routes = Info::get("CacheRoutes");
 
@@ -83,12 +89,18 @@ class DPanel
             "render_map" => $render_map,
             "my_params" => self::my_debug(),
             "workpan" => count(WorkDebug::get()) > 0 ? "inline-block" : "none",
+            "sqlqpan" => self::$queries > 0 ? "inline-block" : "none",
             "cache_routes_color" => $cashe_routes ? "yellowgreen" : "white",
             "path" => self::create_path($path . "/" . $block["data_path"]),
             "cache_routes_text" => $cashe_routes ? " Updated now" : "",
             "route_path" => self::create_path($block["data_path"]),
             "autoload" => is_array(Info::get("Autoload")) ? Info::get("Autoload") : [],
-            "cache" => date(DATE_ATOM, filemtime(HLEB_GLOBAL_DIRECTORY . "/storage/cache/routes/routes.txt"))
+            "templates" => is_array(Info::get("Autoload")) ? Info::get("Templates") : [],
+            "cache" => date(DATE_ATOM, filemtime(HLEB_GLOBAL_DIRECTORY . "/storage/cache/routes/routes.txt")),
+            "orm_report" => $orm_report[0],
+            "orm_time_report" => $orm_report[1],
+            "orm_report_active" => self::$queries,
+            "orm_count" => $orm_report[2],
         ];
     }
 
@@ -104,11 +116,32 @@ class DPanel
 
     }
 
+    private static function create_orm_report(): array
+    {
+        $rows = "";
+        if(class_exists("\Hleb\Main\DataDebug")){
+
+           $data = \Hleb\Main\DataDebug::get();
+           $all_time = 0;
+           foreach($data as $key => $value){
+               $ms = round($value[1], 4);
+               $all_time += round($ms, 4);
+               $rows .= "<div style='padding: 4px; margin-bottom: 4px; background-color: whitesmoke; line-height: 2'>" .
+                   "<div style='display: inline-block; min-width: 16px; color:gray; padding: 0 5px;" .
+                   "' align = 'center'>" . ($key+1) . "</div> <span style='color:gray'>[" .
+                   "<div style='display: inline-block; color:black; min-width: 26px' align='right'>" . $value[3] . ($ms * 1000) .
+                   "</div> ms] " . htmlentities($value[2]) . "</span>&#8195;" . trim($value[0], ";") . ";</div>";
+           }
+        }
+        return [$rows, round($all_time, 4), count($data)];
+    }
+
     public static function print_work_info()
     {
         $data = WorkDebug::get();
 
         if (count($data) > 0) {
+            $right = self::$queries ? 100 : 60;
             require_once "panels/w_header.php";
             foreach ($data as $key => $value) {
                 print "<div style='border: 1px solid #bfbfbf; padding: 10px;'><pre>";
@@ -116,44 +149,43 @@ class DPanel
                 var_dump($value[0]);
                 print "</pre></div>";
             }
-            print "</div>";
+            print "</div>" . PHP_EOL;
+            print "<!-- /WORK DEBUG PANEL -->";
         }
     }
 
     private static function my_debug()
     {
-        $info = MyDebug::all();
-        $result = [];
-        foreach($info as $k=>$inf) {
-            $result[$k] = ['name'=>$k, 'cont'=>'', 'num'=>0];
-            if (is_array($inf)) {
-                $result[$k]['num'] = count($inf);
-                foreach ($inf as $key=>$value) {
-                    $result[$k]['cont'] .=  "<div style='padding: 3px;'><b>" . $key . "</b>: ";
-                    $result[$k]['cont'] .= (is_array($value) ? htmlspecialchars(stripcslashes(json_encode($value))) : $value);
-                    $result[$k]['cont'] .= "</div>";
-                }
-            } else {
-                $to_str = strval($inf);
-                $result[$k]['cont'] .= $to_str;
-                $result[$k]['num'] = "len " . strlen($to_str);
-            }
-        }
-        return $result;
+      $info = MyDebug::all();
+      $result = [];
+      foreach($info as $k=>$inf) {
+          $result[$k] = ['name'=>$k, 'cont'=>'', 'num'=>0];
+          if (is_array($inf)) {
+              $result[$k]['num'] = count($inf);
+              foreach ($inf as $key=>$value) {
+                  $result[$k]['cont'] .=  "<div style='padding: 3px;'><b>" . $key . "</b>: ";
+                  $result[$k]['cont'] .= (is_array($value) ? htmlspecialchars(stripcslashes(json_encode($value))) : $value);
+                  $result[$k]['cont'] .= "</div>";
+              }
+          } else {
+              $to_str = strval($inf);
+              $result[$k]['cont'] .= $to_str;
+              $result[$k]['num'] = "len " . strlen($to_str);
+          }
+      }
+      return $result;
     }
 
     private static function my_links()
     {
         $links = "<span style='display:inline-block; margin: 15px 15px 0 0;color:#9d9d9d;'>" .
             "<a href='https://phphleb.ru/'><span style='color:#9d9d9d;'>phphleb.ru</span></a></span>";
-
         foreach($GLOBALS["HLEB_PROJECT_UPDATES"] as $key => $value) {
             if(stripos($key, "phphleb/") === 0) {
                 $links .= "<div style='display:inline-block; margin: 15px 15px 0 0; white-space: nowrap; color:grey;'>" .
                     "<a href='https://github.com/$key/'><span style='color:#9d9d9d;'>$key</span></a> $value </div>";
             }
         }
-
         return $links;
     }
 
